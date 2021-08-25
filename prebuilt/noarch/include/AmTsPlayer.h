c215098d42bce4bc833561e1c0626221070a6f81
zhipeng.he <zhipeng.he@amlogic.com>
2021-08-25 21:24:42 +0800

    tsplayer: CF2 audio format support flac. [1/1]
    PD#OTT-23023
    
    Problem:
    audio format not support flac.
    
    Solution:
    audio format support flac.
    
    Verify:
    Common
    SourceCode:
    commit 851e9df2ef06e5dfbdfa07c70461f8f965e51f79 (HEAD -> Linux)
    Author: zhipeng.he <zhipeng.he@amlogic.com>
    Date:   Wed Aug 25 21:19:31 2021 +0800
    
    Change-Id: I6bd16d3bb89a2070ce7756e557591ae8962a5019
    Signed-off-by: zhipeng.he <zhipeng.he@amlogic.com>

--- prebuilt/noarch/include/AmTsPlayer.h
+++ prebuilt/noarch/include/AmTsPlayer.h
@@ -204,6 +204,7 @@ typedef enum {
     AV_AUDIO_CODEC_PCM = 8,                // PCM
     AV_AUDIO_CODEC_AC4 = 9,                // AC4
     AV_AUDIO_CODEC_DRA = 10,               // DRA
+    AV_AUDIO_CODEC_FLAC = 11,               // FLAC
     AV_AUDIO_CODEC_MAX,                    // max audio type
 //    AV_AUDIO_CODEC_HEAAC = 8,            // HEAAC
 //    AV_AUDIO_CODEC_AAC_ADTS = 9,         // AAC_ADTS
